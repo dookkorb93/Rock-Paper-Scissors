18794840c955ead30be876177618c8775052cade
dookkorb93 <kolby.heid@gmail.com>
2025-03-08 16:18:32 -0600

    Enter switch statement for function sum

--- javascript.js
+++ javascript.js
@@ -6,9 +6,23 @@ function getComputerChoice(textString) {
 
 const randomWord = getComputerChoice(myString);
 
-//let choice = prompt("Rock, Paper, or Scissors?");
 function getHumanChoice() {
-    return randomWord;
+    const humanChoice = prompt("Rock, Paper, or Scissors?");
+    const answer = humanChoice;
+    switch(answer) {
+        case "Rock":
+            console.log("Rock");
+            break;
+        case "Paper":
+            console.log("Paper");
+            break;
+        case "Scissors":
+            console.log("Scissors");
+            break;
+        default:
+            console.log("Please enter a valid value");
+    }
+    return answer;
 }
 
 console.log(randomWord);
