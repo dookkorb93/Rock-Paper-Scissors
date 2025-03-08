a186f0cae4de7846136112f508c067ab497e8ad3
dookkorb93 <kolby.heid@gmail.com>
2025-03-08 13:09:17 -0600

    Change () to [] to help the code to work

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 function getComputerChoice(textString) {
     const randomString = Math.floor(Math.random() * textString.length);
-    return textString [randomString]
+    return textString[randomString]
 }
-
-const myString = ("Rock", "Paper", "Scissors");
-const randomWord = getComputerChoice[myString];
+const myString = ["Rock", "Paper", "Scissors"];
+const randomWord = getComputerChoice(myString);
