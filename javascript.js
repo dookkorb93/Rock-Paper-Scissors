5ed762f2219f408bd1886bf5799d0a4521ef2715
dookkorb93 <kolby.heid@gmail.com>
2025-03-13 18:11:07 -0500

    Add comment to remind me what I have to do

--- javascript.js
+++ javascript.js
@@ -64,6 +64,8 @@ function playRound(humanChoice, computerChoice) {
         console.log("Please enter a valid value");
 }
 
+// Add return values for above code and then pass function calls as arguments for different function call (log)
+
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
